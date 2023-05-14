e53cf86488b5a42183345b30daae2b5371b854b7
Pesteves2002 <tomasesteves2002@gmail.com>
2023-05-14 22:13:56 +0100

    Add axes and cameras to scene

--- project-B/js/main-script.js
+++ project-B/js/main-script.js
@@ -2,19 +2,57 @@
 /* GLOBAL VARIABLES */
 //////////////////////
 
+var camera, scene, renderer;
 
+var geometry, material, mesh;
+
+var cameras = [];
+
+var currentCamera;
+
+var cameraValues = [
+  [50, 50, 50],
+  [0, 100, 0],
+  [0, 0, 100],
+  [100, 0, 0],
+];
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
-function createScene(){
-    'use strict';
+function createScene() {
+  "use strict";
 
+  scene = new THREE.Scene();
+
+  scene.add(new THREE.AxesHelper(50));
 }
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
 
+function createCameras() {
+  "use strict";
+  cameraValues.forEach((cameraValue) => {
+    createCamera(cameraValue);
+  });
+}
+
+function createCamera(cameraValue) {
+  "use strict";
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
+  camera.position.x = cameraValue[0];
+  camera.position.y = cameraValue[1];
+  camera.position.z = cameraValue[2];
+  camera.lookAt(scene.position);
+
+  cameras.push(camera);
+}
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -27,71 +65,102 @@ function createScene(){
 //////////////////////
 /* CHECK COLLISIONS */
 //////////////////////
-function checkCollisions(){
-    'use strict';
-
+function checkCollisions() {
+  "use strict";
 }
 
 ///////////////////////
 /* HANDLE COLLISIONS */
 ///////////////////////
-function handleCollisions(){
-    'use strict';
-
+function handleCollisions() {
+  "use strict";
 }
 
 ////////////
 /* UPDATE */
 ////////////
-function update(){
-    'use strict';
-
+function update() {
+  "use strict";
 }
 
 /////////////
 /* DISPLAY */
 /////////////
 function render() {
-    'use strict';
-
+  "use strict";
+  renderer.render(scene, currentCamera);
 }
 
 ////////////////////////////////
 /* INITIALIZE ANIMATION CYCLE */
 ////////////////////////////////
 function init() {
-    'use strict';
+  "use strict";
 
+  renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  createScene();
+  createCameras();
+
+  currentCamera = cameras[0];
+
+  render();
+
+  window.addEventListener("keydown", onKeyDown);
+  window.addEventListener("resize", onResize);
 }
 
 /////////////////////
 /* ANIMATION CYCLE */
 /////////////////////
 function animate() {
-    'use strict';
-
+  "use strict";
 }
 
 ////////////////////////////
 /* RESIZE WINDOW CALLBACK */
 ////////////////////////////
-function onResize() { 
-    'use strict';
+function onResize() {
+  "use strict";
 
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  if (window.innerHeight > 0 && window.innerWidth > 0) {
+    currentCamera.aspect = window.innerWidth / window.innerHeight;
+    currentCamera.updateProjectionMatrix();
+  }
 }
 
 ///////////////////////
 /* KEY DOWN CALLBACK */
 ///////////////////////
 function onKeyDown(e) {
-    'use strict';
+  "use strict";
 
+  console.log(currentCamera);
+
+  switch (e.keyCode) {
+    case 49: //1
+      currentCamera = cameras[0];
+      break;
+    case 50: //2
+      currentCamera = cameras[1];
+      break;
+    case 51: //3
+      currentCamera = cameras[2];
+      break;
+  }
+
+  render();
 }
 
 ///////////////////////
 /* KEY UP CALLBACK */
 ///////////////////////
-function onKeyUp(e){
-    'use strict';
-
+function onKeyUp(e) {
+  "use strict";
 }
