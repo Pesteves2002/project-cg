5e8dc4cd7056bff4ce96cd49eb34c6414e531fc2
Pesteves2002 <tomasesteves2002@gmail.com>
2023-05-29 22:00:37 +0100

    Add boilerplate code

---
+++ project-C/js/constants.js
@@ -0,0 +1,28 @@
+const UNIT = 20;
+
+const CLOCK = new THREE.Clock();
+
+const DELTA_MULT = 100;
+
+const FOV = 70;
+
+const MINVIEWDISTANCE = 1;
+
+const MAXVIEWDISTANCE = 10000;
+
+const AXIS = {
+  X: "x",
+  Y: "y",
+  Z: "z",
+};
+
+const PRIMITIVES = {
+  CUBE: "cube",
+  CYLINDER: "cylinder",
+};
+
+// RED X WIDTH
+// Green Y HEIGHT
+// BLUE Z DEPTH
+
+const CAMERAVALUES = [[0, 0, 1000]];
