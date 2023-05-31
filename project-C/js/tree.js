e8bb282e81985908a74478977536c7278c1efd7e
Pesteves2002 <tomasesteves2002@gmail.com>
2023-05-31 17:05:19 +0100

    Add tree

---
+++ project-C/js/tree.js
@@ -0,0 +1,62 @@
+function createTree() {
+  "use strict";
+
+  const trunk = createTrunk();
+  setPosition(trunk, TRUNKVALUES);
+
+  scene.add(trunk);
+}
+
+function createTrunk() {
+  "use strict";
+
+  const group = new THREE.Group();
+
+  const trunk = createObject3D(TRUNKVALUES);
+  const secondTrunk = createSecondTrunk();
+  const leaves = createLeaves();
+
+  trunk.rotation.z = Math.PI / 8;
+
+  group.add(trunk);
+  group.add(secondTrunk);
+  group.add(leaves);
+
+  return group;
+}
+
+function createSecondTrunk() {
+  "use strict";
+
+  const group = new THREE.Group();
+
+  const secondTrunk = createObject3D(TRUNKVALUES2);
+  const secondLeaves = createSecondLeaves();
+
+  group.add(secondTrunk);
+  group.add(secondLeaves);
+
+  secondTrunk.rotation.z = -Math.PI / 4;
+
+  setPosition(group, TRUNKVALUES2);
+
+  return group;
+}
+
+function createLeaves() {
+  "use strict";
+
+  const leaves = createObject3D(LEAFVALUES);
+  setPosition(leaves, LEAFVALUES);
+
+  return leaves;
+}
+
+function createSecondLeaves() {
+  "use strict";
+
+  const leaves = createObject3D(LEAFVALUES2);
+  setPosition(leaves, LEAFVALUES2);
+
+  return leaves;
+}
